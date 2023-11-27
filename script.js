7d7bbcc4012d4f94c4a202a377b3dbdb80b4844c
Nikolas <nikolasrailanmp@gmail.com>
2023-11-27 15:23:25 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const animais = document.getElementById("animais");
-console.log(animais);
+// Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
 
-console.log("teste");
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imagensAnimais);
+
+// Selecione todos os links internos (onde o href começa com #)
+const links = document.querySelectorAll('[href^="#"]');
+console.log(links);
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const h2 = document.querySelector('.animais-descricao h2');
+console.log(h2);
+
+// Selecione o último p do site
+const ps = document.querySelectorAll('p');
+console.log(ps[ps.length-1]);
+
