1482eddb483f8346ba2b0938e6cfce484e995a50
Nikolas <nikolasrailanmp@gmail.com>
2023-11-30 16:00:49 -0300

    scroll suave

--- script.js
+++ script.js
@@ -39,3 +39,32 @@ function initAccordion(){
 }
 
 initAccordion()
+
+function initScrollSuave(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+  function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);  const topo = section.offsetTop;
+    
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+    
+    //forma alternativa
+    // window.scrollTo({
+      //   top: topo,
+      //   behavior: 'smooth'
+      // });
+      
+      
+    }
+    
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    })
+}
+
+initScrollSuave();
